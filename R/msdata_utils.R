40d9e120d24e43a8dbfef657afc18f58e612fbfc
Alexey Stukalov <astukalov@gmail.com>
2019-05-22 03:16:14 +0200

    [+] msdata_utils.R: append_protgroups_info()

---
+++ R/msdata_utils.R
@@ -0,0 +1,80 @@
+# utilities for preparing msdata list
+
+uniprot_fasta_extract_ac <- function(fasta_headers) {
+    str_replace_all(fasta_headers, "(^|;)(?:sp|tr)\\|([a-zA-Z0-9_-]+)\\|\\S+($|;)", "\\1\\2\\3")
+}
+
+agg_protgroup_col <- function(data) {
+  if (is.character(data)) {
+    str_c(unique(data), collapse=';')
+  } else if (is.logical(data)) {
+    any(data) # FIXME what if all?
+  } else {
+    error("don't know how to aggregate ", typeof(data), " type")
+  }
+}
+
+#' @export
+append_protgroups_info <- function(msdata, msdata_wide, proteins_info = NULL,
+                                   fix_protein_info = TRUE,
+                                   import_columns = NULL) {
+    msdata_colgroups <- attr(msdata_wide, "column_groups")
+    pg_df <- dplyr::select(msdata_wide, !!msdata_colgroups$protgroup) %>%
+        dplyr::distinct() %>% dplyr::arrange(protgroup_id)
+    # keep only protgroups with data
+    intensity_dfnames <- str_subset(names(msdata), "intensities$")
+    for (dfname in intensity_dfnames) {
+        pg_df <- dplyr::semi_join(pg_df, msdata[[dfname]])
+    }
+    if (fix_protein_info) {
+        pg_df <- pg_df %>%
+            dplyr::mutate(# fix ACs due to incorrect mqpar parse rules
+                          protein_acs = uniprot_fasta_extract_ac(protein_acs),
+                          majority_protein_acs = uniprot_fasta_extract_ac(majority_protein_acs))
+    }
+    protein2pg_df <- dplyr::bind_rows(
+        dplyr::mutate(expand_protgroup_acs(pg_df, acs_col="majority_protein_acs"),
+                      is_majority = TRUE) %>%
+        dplyr::rename(protein_ac = majority_protein_ac),
+        dplyr::mutate(expand_protgroup_acs(pg_df, acs_col="protein_acs"),
+                      is_majority = FALSE)) %>%
+        dplyr::group_by(protgroup_id, protein_ac) %>%
+        dplyr::summarise(is_majority = any(is_majority)) %>%
+        dplyr::ungroup()
+    if (!is.null(proteins_info)) {
+        proteins_df <- semi_join(proteins_info, protein2pg_df) %>%
+            dplyr::left_join(dplyr::select(dplyr::filter(protein2pg_df, is_majority),
+                                           protein_ac, protgroup_id))
+        if (fix_protein_info) {
+            # fix gene and protein names using FASTA information
+            fix_cols <- c("protein_names" = "protein_name", "gene_names" = "gene_name")
+        } else {
+            fix_cols <- c()
+        }
+        agg_cols <- fix_cols
+        if (!is.null(import_columns)) {
+            agg_cols <- c(agg_cols, import_columns)
+        }
+        fixpg_df <- dplyr::left_join(dplyr::select(pg_df, protgroup_id),
+                                     dplyr::arrange(proteins_df, protein_ac) %>%
+                                     dplyr::group_by(protgroup_id) %>%
+                                     dplyr::summarise_at(agg_cols, agg_protgroup_col) %>%
+                                     dplyr::ungroup())
+        if (any(fixpg_df$protgroup_id != pg_df$protgroup_id)) {
+            warning("Incorrect fixed protgroups info, skipping")
+        } else {
+            for (fix_col in intersect(fix_cols, colnames(pg_df))) {
+                pg_mask <- !is.na(fixpg_df[[fix_col]]) & is.na(pg_df[[fix_col]])
+                if (any(pg_mask)) {
+                    pg_df[pg_mask, fix_col] <- fixpg_df[pg_mask, fix_col]
+                }
+            }
+            for (imp_col in import_columns) {
+                pg_df[, imp_col] <- fixpg_df[[imp_col]]
+            }
+        }
+    }
+    return(modifyList(msdata, list(protgroups = pg_df,
+                                   protein2protgroup = protein2pg_df,
+                                   proteins = proteins_df)))
+}
